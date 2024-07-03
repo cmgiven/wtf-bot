6428ad621a831bd2fe02cf0c2aa4b776ce741256
Max Mazzocchi <114442717+MaxMazzocchiUSDS@users.noreply.github.com>
2024-07-03 14:47:09 -0400

    Dockerization (#11)
    * Added some CLI scripts for testing
    
    * Removed CF files
    
    * Added Docker files
    
    * Removed proxy env
    
    * Added HOST/PORT definitions
    
    * Removed blank line
    
    * Fixed Dockerfile
    
    * Status API
    
    * README update
    
    * Converted settings to JSON/env, imported through docker-compose
    
    * Fixed env name
    
    * Reference to example-settings.env
    
    * Change to OMB Connect
    
    * Removed temp file

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: RUBYOPT=-W:no-deprecated bundle exec rackup config.ru -p $PORT
--- src/app.rb
+++ src/app.rb
@@ -1,6 +1,7 @@
 require_relative 'text_api'
 require_relative 'slack_api'
 require_relative 'github_api'
+require_relative 'status_api'
 
 module WtfBot
   def self.app
@@ -15,6 +16,10 @@ module WtfBot
 
       map '/github' do
         run GithubApi
+      end
+
+      map '/status' do
+        run StatusApi
       end
     end
   end
